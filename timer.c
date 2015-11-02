94dfd30f9360a525c1a113668e2580fe30fe6f0e
JAbrial <abrial.jean@gmail.com>
2015-11-02 11:34:38 +0100

    Update timer.c

--- timer.c
+++ timer.c
@@ -1,13 +1,22 @@
-void config_TIMX()
+void ConfigTIM2 ()			
 {
-  RCC->APB1ENR |=1<<0; //cfg timer
-  TIM2->CR1 &=~(0xFE);
-  TIM2->PSC=1;
-  TIM2->ARR=399;
-  //configuration interne 
-  TIM2->CR1|=1<<0;
-  TIM2->SR |=1<<0;
-  while((TIM2->SR & 1<<0)!=1<<0){};
-  TIM2->SR &=~(0x01);
-  TIM2->CR1 &=~(0x01);
+    NVIC->ISER[0] |= 1<<28;                     //TIM2_IRQHandler = 28
+    RCC->APB1ENR |= 1<<0;                       //configure l'horloge de TIM2
+    TIM2->PSC = 1;                            //Configurer TIM2 avec : PSC+1 * ARR+1 = Fclk/Fctn avec Fclk = 16MHz
+    TIM2->ARR = 399;                           
+    TIM2->CR1 |= 0x00;                          //"Eteint" le TIMER
+    TIM2->DIER |= 0x01;                         //Autorise l'interruption
+}
+
+void TIM2_IRQHandler (void)
+{
+    TIM2->SR &= ~(0x01);
+     if (i == 1)
+      {
+        i = 0;
+      }
+      else
+      {        
+        i = 1;
+      }
 }
