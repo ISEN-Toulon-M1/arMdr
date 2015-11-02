3ca78c762ee2aeb007bac3d861dc159919db8e2b
JAbrial <abrial.jean@gmail.com>
2015-11-02 11:17:42 +0100

    Config ARR & PSC

--- timer.c
+++ timer.c
@@ -1,10 +1,10 @@
-void config_TIMX(int val_psc)
+void config_TIMX()
 {
   RCC->APB1ENR |=1<<0; //cfg timer
   TIM2->CR1 &=~(0xFE);
-  TIM2->PSC=val_psc;
-  TIM2->ARR=9999;
-  //config interne (voir feuille)
+  TIM2->PSC=1;
+  TIM2->ARR=399;
+  //configuration interne 
   TIM2->CR1|=1<<0;
   TIM2->SR |=1<<0;
   while((TIM2->SR & 1<<0)!=1<<0){};
